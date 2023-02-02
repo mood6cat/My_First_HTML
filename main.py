25e4a60546799e90186f39c64b5e8a751f489905
dant9 <dant982@mail.ru>
2023-02-02 20:12:52 +0500

    404 not found??

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ def page_index():
     candidate = utils.get_all()
     return render_template("list.html", candidate=candidate)
 
+
 @app.route('/candidate/<int:pk>')
 def img_candidate(pk):
     candidate = utils.get_by_pk(pk)
     return render_template('card.html', candidate=candidate)
-
-
 
 
 if __name__ == '__main__':
